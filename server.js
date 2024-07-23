878d9a1e4cc9fcda3a41d6b7ed58e2a8621ded72
Ndubuisi Deborah <114514927+Code-Sorceress@users.noreply.github.com>
2024-07-23 09:39:41 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,46 @@
+//IMPORT FOR PACKAGES
+import "express-async-errors";
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+import morgan from "morgan";
+import mongoose from "mongoose";
+
+//CUSTOM IMPORTS COMING FROM FILE
+import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
+
+//middleware
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.use("/api/v1/jobs", jobRouter);
+app.use("/api/v1/auth", authRouter);
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5100;
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
